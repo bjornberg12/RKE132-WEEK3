d4b2d409ca4408b3a8ca325a75c958ca635cc84a
Hugo L <hugo.liivaleht@tktk.ee>
2024-09-20 12:07:11 +0300

    add week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter a number");
+
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+// arv on paaris voi paaritu 
+
+int result = userNum % 2;
+
+Console.WriteLine(result);
+
+if (result != 0)
+{
+    Console.WriteLine("user num is odd.");
+
+} else
+{
+    Console.WriteLine("user num is even.");
+}
---
+++ Week3IfAndSwitch/Program.cs
@@ -0,0 +1,10 @@
+﻿//modulus 
+
+int a = 25;
+int b = 8;
+
+int c  = b % a;
+
+Console.WriteLine(c);
+
+
